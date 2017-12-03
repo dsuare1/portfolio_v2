f5cb91efd90707970635a5d2bf2a3fa138f79f0b
derrick <dsuarez@zilkr.io>
2017-12-02 19:22:04 -0600

    pv_3 - added default copy section on landing page; ready to start hover functionality

---
+++ src/js/components/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Person() {
+  return (
+    <div id="person-container">
+      <p>Father, husband, and Node.js developer passionate about building testable, scalable software, with a focus on APIs and backend systems.</p>
+      <p>Originally from Louisiana, I&apos;ve been a resident of the great city of Austin, TX since mid-2014. I love to travel with my wife, with whom I was lucky enough to live in Madrid, Spain for two years, and I can&apos;t wait to share the world with my daughter.</p>
+      <p>I enjoy listening to music and attending live shows, woodworking in my free time, and playing golf. Two fun facts about me: I ran with the bulls in Pamplona, Spain during the San Fermines festival in 2012 and have performed five wedding ceremonies.</p>
+    </div>
+  );
+}
+
+export default Person;
