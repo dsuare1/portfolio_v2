9a8a393d4551e02a37402f03135ae315616792f4
derrick <dsuarez@zilkr.io>
2018-01-01 14:35:03 -0600

    pv2_13 - added remaining projects to portfolio section

--- src/js/components/Portfolio.js
+++ src/js/components/Portfolio.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import traLandingPage from '../../assets/img/The-Round-About-Landing-Page.png';
-import seinfeld from '../../assets/img/seinfeld.png';
+import traLandingPage from '../../assets/img/tra_landing_page.png';
+import seinfeldLandingPage from '../../assets/img/seinfeld_landing_page.png';
+import pickUpLandingPage from '../../assets/img/pickup_landing_page.png';
+import friendFinderLandingPage from '../../assets/img/friend_finder_landing_page.png';
 
 function Portfolio() {
   return (
@@ -13,7 +15,17 @@ function Portfolio() {
       </div>
       <div className="portfolio-item">
         <a href="http://suarez-seinfeld-trivia.herokuapp.com/" className="portfolio-link" target="_blank" rel="noopener noreferrer"><p>&copy;Seinfeld Trivia Game</p>
-          <img src={seinfeld} alt="Seinfeld Trivia Game" />
+          <img src={seinfeldLandingPage} alt="Seinfeld Trivia Game" />
+        </a>
+      </div>
+      <div className="portfolio-item">
+        <a href="http://bjrd-pickup.herokuapp.com/" className="portfolio-link" target="_blank" rel="noopener noreferrer"><p>PickUp</p>
+          <img src={pickUpLandingPage} alt="Pick Up" />
+        </a>
+      </div>
+      <div className="portfolio-item">
+        <a href="http://suarez-friend-finder.herokuapp.com/" className="portfolio-link" target="_blank" rel="noopener noreferrer"><p>Friend Finder</p>
+          <img src={friendFinderLandingPage} alt="Friend Finder" />
         </a>
       </div>
     </div>
